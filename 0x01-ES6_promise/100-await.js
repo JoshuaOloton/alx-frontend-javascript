23870a1bc16923962052d67d57d82331fe369745
Joshua Oloton <olotonjoshua@gmail.com>
2023-07-02 15:15:21 +0100

    added catch for errors

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,9 +1,14 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  const result = {
-    photo: await uploadPhoto(),
-    user: await createUser(),
-  };
-  return result;
+  let photo;
+  let user;
+  try {
+    photo = await uploadPhoto();
+    user = await createUser();
+  } catch (error) {
+    photo = null;
+    user = null;
+  }
+  return { photo, user };
 }
