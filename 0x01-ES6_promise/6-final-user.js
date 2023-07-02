db47b62672d42a5c500a8a88781cc602f5fa9763
Joshua Oloton <olotonjoshua@gmail.com>
2023-07-02 11:24:09 +0100

    adjusted more errors

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,15 +2,16 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
+  const promisesArr = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
   const arr = [];
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+  return Promise.allSettled(promisesArr)
     .then((results) => {
-      results.forEach(result => {
+      for (const result of results) {
         arr.push({
           status: result.status,
           value: result.status === 'fulfilled' ? result.value : result.status,
-        })
-      })
+        });
+      }
       return arr;
     });
 }
